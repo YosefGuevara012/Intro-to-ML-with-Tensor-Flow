e9206831b8b6135de82812db2f6b97936179feb9
YOSEF GUEVARA <yosef.guevara012@hotmail.com>
2022-04-04 18:18:03 -0500

    Unsupervised Learning, Clustering, Feature Scaling

--- 3. Unsupervised Learning/1. Clustering/3. Feature Scaling Example/tests2.py
+++ 3. Unsupervised Learning/1. Clustering/3. Feature Scaling Example/tests2.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.cluster import KMeans
 from IPython.display import Image
-from sklearn.datasets.samples_generator import make_blobs
-
+# from sklearn.datasets.samples_generator import make_blobs
+from sklearn.datasets import make_blobs
 
 
 def check_q1(stuff):

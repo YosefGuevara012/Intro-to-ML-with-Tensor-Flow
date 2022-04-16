9c53be09e8f46ced2de4b29e4bfdfba667e1f619
YOSEF GUEVARA <yosef.guevara012@hotmail.com>
2022-04-16 17:01:00 -0500

    3. Intro to NN, Project image classifier, predict.py function everything verified

--- 2. Intro to NN with TensorFlow/Project image classifier/predict.py
+++ 2. Intro to NN with TensorFlow/Project image classifier/predict.py
@@ -10,10 +10,10 @@ from PIL import Image
 # Argument definition
 
 parser = argparse.ArgumentParser(description = "Predict which class a flower in an image belongs to")
-parser.add_argument('-i', '--image_path', default = './test_images/hard-leaved_pocket_orchid.jpg' , help = 'Image path')
-parser.add_argument('-m', '--checkpoint', help = 'Model checkpoint')
-parser.add_argument('-k', '--top_k', default = 5, type = int, help = 'Top K most likely classes')
-parser.add_argument('-c', '--category_names', default = 'label_map.json', help = "Maps the real categories name')
+parser.add_argument('-i', '--image_path', default = "./test_images/hard-leaved_pocket_orchid.jpg", help = 'Image path')
+parser.add_argument('-m', '--checkpoint', help = "Model checkpoint")
+parser.add_argument('-k', '--top_k', default = 5, type = int, help = "Top K most likely classes")
+parser.add_argument('-c', '--category_names', default = 'label_map.json', help = "Maps the real categories name")
 args = parser.parse_args()
 
 # Argument assignment
@@ -24,7 +24,7 @@ top_k = args.top_k
 label_map = args.category_names
 
 # Mapping from label to category name
-with open('label_map', 'r') as f:
+with open(label_map, 'r') as f:
     class_names = json.load(f)
 
 # Load the Keras Model
